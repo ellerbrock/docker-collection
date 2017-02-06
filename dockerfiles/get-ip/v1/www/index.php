b9bf0ce0d313d77d91c99217b237d24c2cff32bb
Maik Ellerbrock <opensource@frapsoft.com>
2017-02-06 17:31:47 +0100

    fix(php): add missing semicolon for v2

--- dockerfiles/get-ip/v1/www/index.php
+++ dockerfiles/get-ip/v1/www/index.php
@@ -1,5 +1,5 @@
 <?php
   echo "<h1>Version 1</h1>";
-  echo $_SERVER['SERVER_ADDR'];
+  echo "<h2>" . $_SERVER['SERVER_ADDR'] . "</h2>";
 ?>
 
--- dockerfiles/get-ip/v2/www/index.php
+++ dockerfiles/get-ip/v2/www/index.php
@@ -1,5 +1,5 @@
 <?php
-  echo "<h1>Version 2</h1>"
-  echo $_SERVER['SERVER_ADDR'];
+  echo "<h1>Version 2</h1>";
+  echo "<h2>" . $_SERVER['SERVER_ADDR'] . "</h2>";
 ?>
 
